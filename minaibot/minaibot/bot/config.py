c6461b6c8e31f6c09ae2a1bdd59649c474ac053f
nikita-minai <nikita.minai@ya.ru>
2024-05-18 22:14:03 +0300

    Delete minaibot directory

--- minaibot/minaibot/bot/config.py
+++
@@ -1,3 +0,0 @@
-DEBUG_MESSAGE = True
-TOKEN = "5841150507:AAGI_bohSAg-hGp8do4RzpGVs0VQGKFexds"
-PERMISSION_DENIED_MESSAGE = "К сожалению, %s, у Вас нет доступа к этому боту.\nЗапросить доступ можно у его создателя - %s"
--- minaibot/minaibot/bot/myserver.py
+++
@@ -1,6 +0,0 @@
-from minaibot.core.server import Server
-
-
-class MySever(Server):
-    def check_permission(self, *args, **kwargs):
-        return False
